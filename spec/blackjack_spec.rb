fd847cbe0628d0960be850ad038d8c25b47ecd02
codyss <cody.schwarz@gmail.com>
2015-10-10 19:18:36 -0400

    Done.

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -77,11 +77,7 @@ describe "#hit?" do
     end
   end
 
-  it "calls on #prompt_user then #get_user_input" do
-    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-    expect(self).to receive(:get_user_input).and_return("s")
-    hit?(7)
-  end
+
 
   it "returns an integer which is the card total" do
     expect(self).to receive(:get_user_input).and_return("s")
