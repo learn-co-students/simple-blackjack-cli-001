695e09624b20a0a5ef2555257e4e40334402e3b8
wolfsandle <wolfsandle@gmail.com>
2016-08-06 20:49:32 -0700

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,64 @@
+$exit_signal=0
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  new_card = rand(1..11)
+  puts "You were dealt #{new_card}"
+  return new_card
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp.strip
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  
+  if card_total>21
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  elsif card_total<21
+    puts "You have ended the game with #{card_total}!"
+  elsif card_total==21
+    puts "Blackjack!"
+    puts "Congrats! You are a big winner you got #{card_total}!"
+  end
 end
 
 def initial_round
-  # code #initial_round here
+  first_card = deal_card
+  second_card = deal_card
+  first_round = first_card + second_card
+  display_card_total(first_round)
+  return first_round
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+    $exit_signal = 1
+    card_total
+  elsif
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Sorry, you have chosen an invalid command, please try again."
+  get_user_input
 end
 
 #####################################################
@@ -39,6 +66,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total >= 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+    if $exit_signal == 1
+      break
+    end
+  end
+    end_game(card_total)
 end
-    
